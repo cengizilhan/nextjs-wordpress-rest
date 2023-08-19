046cee37ca5d0e3a0d35ad58bb1e37e14aec5480
Cengiz İlhan <info@cengizilhan.com>
2023-08-19 20:15:44 +0300

    ..

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Product from '@/app/components/products'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Anasayfa
+        This website has been built using the Next.js and Tailwind CSS frameworks. It connects to the Wordpress Rest API to fetch and display a list of pages.
     </main>
   )
 }
